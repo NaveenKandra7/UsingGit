9c48130c033b1b75c9742056f12eb6a61ed3ed27
NaveenKandra7 <kandranaveen447@gmail.com>
2017-05-23 00:20:43 -0700

    madLibs commit

---
+++ madLibs.py
@@ -0,0 +1,20 @@
+#!/bin/python
+
+import pdb
+
+print "Have fun reading the story"
+
+noun = raw_input("Enter a Name:")
+
+verb = raw_input("Enter a verb:")
+
+adjective = raw_input("Enter a Adjective:")
+
+game = raw_input("Enter a game:")
+#Start the story
+print "\n"
+print "This is the start of the story," + noun + " is a stupid Engineer who works \
+day and night for his company without getting paid for much of his work. Because of his \
+" + adjective + " nature. I am surprised when i came to know that he has a hobby. \
+Yes, he has a hobby. He plays " + game + ". That's cool, isn't it? What is not cool \
+about this guy is, he " + verb + " and snores while sleeping"
