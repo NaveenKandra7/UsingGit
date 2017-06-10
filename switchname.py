cd5de7b2f340711bf901b4bb5f0da616eff7453d
NaveenKandra7 <kandranaveen447@gmail.com>
2017-06-10 10:37:41 -0700

    code to change switchname

---
+++ switchname.py
@@ -0,0 +1,33 @@
+#!/bin/python
+
+import pexpect
+import pdb
+import sys, re
+class FCoE_Helper():
+
+	def ConnectToSwitch(self, ip, sw_name):
+		self.sw = pexpect.spawn('ssh admin@'+ip)
+		r = self.sw.expect([pexpect.TIMEOUT, '[P/p]assword:','(yes/no)'])
+		#pdb.set_trace()
+		if r == 0 :
+			print "Not able to reach the switch"
+		elif r == 2 :
+			self.sw.sendline("yes")
+			r1 = self.sw.expect([pexpect.TIMEOUT, '[P/p]assword:'])
+			if r1 == 0 :
+				print "Not able to reach the switch after giving yes"
+
+
+		self.sw.sendline('nbv_12345')
+		
+		self.sw.expect([pexpect.TIMEOUT, '#'])
+		self.sw.sendline('terminal length 0')
+		self.sw.expect([pexpect.TIMEOUT, '#'])
+		self.sw.sendline('conf')
+		self.sw.expect([pexpect.TIMEOUT, '#'])
+		
+		self.sw.sendline('switchname '+ sw_name)
+		self.sw.expect([pexpect.TIMEOUT, '#'])
+		
+fh = FCoE_Helper()
+fh.ConnectToSwitch(ip='10.127.125.24', sw_name = 'MDSNG')
